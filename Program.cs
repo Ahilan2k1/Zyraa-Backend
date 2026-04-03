3e276cfd366cf6d5044cd97305df46c92ce67895
Poojashree Elangovan <poojashreeelangovan@Poojashrees-MacBook-Air.local>
2026-04-04 01:16:14 +0530

    demo check

--- Program.cs
+++ Program.cs
@@ -135,7 +135,7 @@ if (!Directory.Exists(dbPath))
 // Health check
 app.MapGet("/api/health", () => Results.Ok(new
 {
-    status = "Zyraa",
+    status = "Zyraa healthy is good",
     time = DateTime.UtcNow
 }));
 
